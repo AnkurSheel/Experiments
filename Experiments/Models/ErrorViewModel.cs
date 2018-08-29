77c8ce4a8f139d1dc618568771d91f6f269953fa
Ankur Sheel <ankur.sheel@pushpay.com>
2018-08-29 15:07:01 +1200

    Add project files.

---
+++ Experiments/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Experiments.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
