a0463e62aa7d4df1fb23c3c7ee0774256189d325
Ankur Sheel <ankur.sheel@gmail.com>
2018-09-05 11:34:36 +1200

    add webpack configuration file

---
+++ Experiments/ClientApp/webpack.config.js
@@ -0,0 +1,29 @@
+﻿const path = require('path');
+
+module.exports = (env = {}, argv = {}) => {
+
+	const config = {
+		mode: argv.mode || 'development', // we default to development when no 'mode' arg is passed
+		entry: {
+			main: './js/main.js'
+		},
+		output: {
+			filename: '[name].js',
+			path: path.resolve(__dirname, '../wwwroot/dist'),
+			publicPath: "/dist/"
+		},
+		module: {
+			rules: [
+				{
+					test: /\.css$/,
+					use: [
+						'style-loader',
+						'css-loader'
+					]
+				}
+			]
+		}
+	}
+
+	return config;
+};
