41e34aec4213e0cd75b9d34df32fa309405932c3
Ankur Sheel <ankur.sheel@gmail.com>
2018-09-05 12:05:38 +1200

    enable hot module replacement

--- Experiments/Startup.cs
+++ Experiments/Startup.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.IO;
+
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -36,6 +39,10 @@ namespace Experiments
 		{
 			if (env.IsDevelopment())
 			{
+				app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+				{
+					ProjectPath = Path.Combine(Directory.GetCurrentDirectory(), "ClientApp"), HotModuleReplacement = true
+				});
 				app.UseDeveloperExceptionPage();
 			}
 			else
