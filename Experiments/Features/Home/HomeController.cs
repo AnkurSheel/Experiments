9a7986420af460ba6415d7c548fd78a583ec984b
Ankur Sheel <ankur.sheel@gmail.com>
2018-09-13 09:54:49 +1200

    remove unneccesary files

--- Experiments/Features/Home/HomeController.cs
+++ Experiments/Features/Home/HomeController.cs
@@ -1,41 +1,12 @@
-﻿using System.Diagnostics;
-
-using Experiments.Models;
-
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Experiments.Features.Home
 {
-	public class HomeController : Controller
-	{
-		public IActionResult Index()
-		{
-			return View();
-		}
-
-		public IActionResult About()
-		{
-			ViewData["Message"] = "Your application description page.";
-
-			return View();
-		}
-
-		public IActionResult Contact()
-		{
-			ViewData["Message"] = "Your contact page.";
-
-			return View();
-		}
-
-		public IActionResult Privacy()
-		{
-			return View();
-		}
-
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
 }
