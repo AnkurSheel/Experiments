5c9399c4d5a12a019dbfc3167043651ba757a3c1
Ankur Sheel <ankur.sheel@gmail.com>
2018-09-13 12:11:45 +1200

    modify webpack to compile tsx files

--- Experiments/ClientApp/webpack.config.js
+++ Experiments/ClientApp/webpack.config.js
@@ -15,6 +15,10 @@ module.exports = (env = {}, argv = {}) => {
             path: path.resolve(__dirname, '../wwwroot/dist'),
             publicPath: "/dist/"
         },
+		resolve: {
+			// Add `.ts` and `.tsx` as a resolvable extension.
+			extensions: [".ts", ".tsx", ".js"]
+		},
         plugins: [
             new MiniCssExtractPlugin({
                 filename: 'styles.css'
@@ -28,10 +32,19 @@ module.exports = (env = {}, argv = {}) => {
                         isProd ? MiniCssExtractPlugin.loader : 'style-loader',
                         'css-loader'
                     ]
+				},
+				{
+					test: /\.tsx?$/,
+					use: 'awesome-typescript-loader',
+					exclude: /node_modules/
                 }
             ]
         }
     }
+
+	if (!isProd) {
+		config.devtool = 'eval-source-map';
+	}
 
     return config;
 };
